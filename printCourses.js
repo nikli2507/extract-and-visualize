3db6167958cf49f4e33d1b7f1d24fff30d5b91d4
nikli2507 <nik.f@gmx.at>
2023-03-31 08:45:29 +0200

    added dummy navbar and visualized course titles as accordions

--- printCourses.js
+++ printCourses.js
@@ -1,25 +1,51 @@
 function printCourses(courses) {
 
-    const divContainer = document.createElement("div");
-    divContainer.classList.add("container")
-    divContainer.classList.add("text-center")
+    const accordion = document.createElement("div");
+    accordion.classList.add("accordion")
+    accordion.setAttribute("id", "coursesAccordion")
 
     for (let i=0; i<courses.length; i++) {
         
-        const divRow = document.createElement("div");
-        divRow.classList.add("row");
+        const accordionItem = document.createElement("div");
+        accordionItem.classList.add("accordion-item");
         
-        const divCol = document.createElement("div");
-        divCol.classList.add("col");
+        const h2 = document.createElement("h2");
+        h2.classList.add("accordion-header");
 
-        const text = document.createTextNode(courses[i]);
+        const button = document.createElement("button");
+        button.classList.add("accordion-button");
+        button.classList.add("collapsed");
+        button.setAttribute("type", "button");
+        button.setAttribute("data-bs-toggle", "collapse");
+        button.setAttribute("data-bs-target", "#collapse"+i);
+        button.setAttribute("aria-expanded", "false");
+        button.setAttribute("aria-controls", "collapse"+i);
+
+        const heading = document.createTextNode(courses[i]);
+
+        const collapse = document.createElement("div");
+        collapse.setAttribute("id", "collapse"+i);
+        collapse.classList.add("accordion-collapse");
+        collapse.classList.add("collapse");
+        collapse.setAttribute("data-bs-parent", "#coursesAccordion");
+
+        const accordionBody = document.createElement("div");
+        accordionBody.classList.add("accordion-body");
+
+        const text = document.createTextNode("sample description");
         
-        divCol.appendChild(text);
-        divRow.appendChild(divCol);
-        divContainer.appendChild(divRow);
+        button.appendChild(heading);
+        h2.appendChild(button);
+        accordionItem.appendChild(h2);
+
+        accordionBody.appendChild(text);
+        collapse.appendChild(accordionBody);
+        accordionItem.appendChild(collapse);
+
+        accordion.appendChild(accordionItem);
 
     }
 
-    document.body.appendChild(divContainer)
+    document.body.appendChild(accordion)
     
 }
